9f6bcdbafef22ca086f14c7b6052f1f773104536
AGESA <collinsagesa2000@gmail.com>
2019-10-24 17:58:43 +0300

    Implicit intent Receiver

---
+++ app/src/main/java/com/example/implicitintentsreceiver/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.implicitintentsreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        if (uri != null) {
+            String uri_string = "URI: " + uri.toString();
+            TextView textView = findViewById(R.id.text_uri_message);
+            textView.setText(uri_string);
+        }
+        else {
+            Toast.makeText(this, "Unable to detect the url", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
